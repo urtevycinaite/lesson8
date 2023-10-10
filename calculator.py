7d93f94913db3b99984f3247fd5dd62e7eca2b52
Urte Vycinaite <urte.vycinaite@gmail.com>
2023-10-10 19:59:14 +0300

    add calculator statement

---
+++ calculator.py
@@ -0,0 +1,20 @@
+first_number = int(input("Enter your first number:"))
+second_number = int(input("Enter your second number:"))
+symbol = input("Enter symbol:")
+
+if symbol == "+":
+    print(first_number+second_number)
+elif symbol == "-":
+    print(first_number-second_number)
+elif symbol == "/":
+    print(first_number/second_number)
+elif symbol == "-":
+    print(first_number-second_number)
+else:
+    print("This mean cannot be used")
+    
+
+
+
+
+
